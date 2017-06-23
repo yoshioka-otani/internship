1ceb5d1cbf1c5f186e74a9c7eb448359b2d5455b
Shohei Yoshida <qrgl15@gmail.com>
2017-06-23 12:45:32 +0000

    update

--- assets/js/voice.js
+++ assets/js/voice.js
@@ -14,6 +14,7 @@
             $(".ishiduka-article").css('display', 'block');
             $(".hasegawa-article").css('display', 'none');
             $(".hanashita-article").css('display', 'none');
+            $(".ishiduka:after").css('display', 'block');
         }else
             $(".ishiduka-article").css('display', 'none');
     });
@@ -28,3 +29,18 @@
             $(".hanashita-article").css('display', 'none');
     });
  });
+ $(document).ready(function(){
+     $(".ishiduka").mouseover(function(){
+        $(".ishiduka p").css('color', '#000000 !important');
+    });
+});
+
+$(window).on('click load resize', function(){
+    // var m_height = 0;
+    // for(var i =0 ; i<=3 ; i++){
+    //     m_height += $('.ishiduka-article > p:eq(i)', '#voice').height();
+    // }
+    var m_height = $('.ishiduka-article > p:eq(0)', '#voice').height() + $('.ishiduka-article > p:eq(1)', '#voice').height() + $('.ishiduka-article > p:eq(2)', '#voice').height() + $('.ishiduka-article > p:eq(3)', '#voice').height();
+    console.log(m_height);
+  $('.ishiduka-article', '#voice').css('height', m_height + 90 +'px' );
+});
