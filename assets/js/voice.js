5cd9a22c8643fc1043aa9cedf1910c7dfc3b5070
Shohei Yoshida <qrgl15@gmail.com>
2017-06-24 06:58:07 +0000

    update

--- assets/js/voice.js
+++ assets/js/voice.js
@@ -29,18 +29,19 @@
             $(".hanashita-article").css('display', 'none');
     });
  });
- $(document).ready(function(){
-     $(".ishiduka").mouseover(function(){
-        $(".ishiduka p").css('color', '#000000 !important');
+$(function() {
+    $("div .ishiduka").hover(function () {
+        console.log("unko");
+        $(".ishiduka p:eq(0)").css('color','#ffffff');
+        $(".ishiduka p:eq(1)").css('color','#ffffff');
+    },function(){
+        $(".ishiduka p:eq(0)").css('color','#00FF66');
+        $(".ishiduka p:eq(1)").css('color','#00FF66');
     });
 });
 
 $(window).on('click load resize', function(){
-    // var m_height = 0;
-    // for(var i =0 ; i<=3 ; i++){
-    //     m_height += $('.ishiduka-article > p:eq(i)', '#voice').height();
-    // }
     var m_height = $('.ishiduka-article > p:eq(0)', '#voice').height() + $('.ishiduka-article > p:eq(1)', '#voice').height() + $('.ishiduka-article > p:eq(2)', '#voice').height() + $('.ishiduka-article > p:eq(3)', '#voice').height();
-    console.log(m_height);
   $('.ishiduka-article', '#voice').css('height', m_height + 90 +'px' );
+  $('#overview').css('margin-top', m_height + 110 + 'px');
 });
