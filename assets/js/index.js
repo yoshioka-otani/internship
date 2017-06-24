657642c025d6e631884e6e2c8577120015d24c33
Otaki <a1a1a2a4@gmail.com>
2017-06-24 22:42:21 +0000

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,35 @@ $(function(){
             $(".bar_menu").css('display', 'none');
     });
  });
+ $(function(){
+    $(".business img:eq(0)").on("click", function() {
+        if($(".ac-branding").css('display') == 'none'){
+            $(".ac-branding").css('display', 'block');
+            $(".ac-inbound").css('display', 'none');
+            $(".ac-innovation").css('display', 'none');
+        }else{
+            $(".ac-branding").css('display', 'none');
+        }
+    });
+    $(".business img:eq(1)").on("click", function() {
+        if($(".ac-inbound").css('display') == 'none'){
+            $(".ac-inbound").css('display', 'block');
+            $(".ac-branding").css('display', 'none');
+            $(".ac-innovation").css('display', 'none');
+        }else{
+            $(".ac-inbound").css('display', 'none');
+        }
+    });
+    $(".business img:eq(2)").on("click", function() {
+        if($(".ac-innovation").css('display') == 'none'){
+            $(".ac-innovation").css('display', 'block');
+            $(".ac-branding").css('display', 'none');
+            $(".ac-inbound").css('display', 'none');
+        }else{
+            $(".ac-innovation").css('display', 'none');
+        }
+    });
+ });
 // $(function(){
 //     $(".people").scroll(function() {
 //         if ($(".people").scrollLeft() >= ($("body").width() - $(".people").width())) {
