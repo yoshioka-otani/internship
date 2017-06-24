433e8108c5daafab12c206ed53107185b340f206
Shohei Yoshida <qrgl15@gmail.com>
2017-06-24 13:31:35 +0000

    update

--- assets/js/voice.js
+++ assets/js/voice.js
@@ -1,32 +1,47 @@
- $(function(){
-    $(".hasegawa").on("click", function() {
-        if($(".hasegawa-article").css('display') == 'none'){
-            $(".hasegawa-article").css('display', 'block');
-            $(".ishiduka-article").css('display', 'none');
-            $(".hanashita-article").css('display', 'none');
-        }else
-            $(".hasegawa-article").css('display', 'none');
-    });
- });
+
  $(function(){
     $(".ishiduka").on("click", function() {
         if($(".ishiduka-article").css('display') == 'none'){
+            $(".ishiduka .triangle").css('display', 'block');
+            $(".hanashita .triangle").css('display', 'none');
+            $(".hasegawa .triangle").css('display', 'none');
             $(".ishiduka-article").css('display', 'block');
             $(".hasegawa-article").css('display', 'none');
             $(".hanashita-article").css('display', 'none');
-            $(".ishiduka:after").css('display', 'block');
-        }else
+        }else{
             $(".ishiduka-article").css('display', 'none');
+            $(".ishiduka .triangle").css('display', 'none');
+        }
+    });
+ });
+  $(function(){
+    $(".hasegawa").on("click", function() {
+        if($(".hasegawa-article").css('display') == 'none'){
+            $(".hasegawa .triangle").css('display', 'block');
+            $(".ishiduka .triangle").css('display', 'none');
+            $(".hanashita .triangle").css('display', 'none');
+            $(".hasegawa-article").css('display', 'block');
+            $(".ishiduka-article").css('display', 'none');
+            $(".hanashita-article").css('display', 'none');
+        }else{
+            $(".hasegawa .triangle").css('display', 'none');
+            $(".hasegawa-article").css('display', 'none');
+        }
     });
  });
  $(function(){
     $(".hanashita").on("click", function() {
         if($(".hanashita-article").css('display') == 'none'){
+            $(".hanashita .triangle").css('display', 'block');
+            $(".ishiduka .triangle").css('display', 'none');
+            $(".hasegawa .triangle").css('display', 'none');
             $(".hanashita-article").css('display', 'block');
             $(".hasegawa-article").css('display', 'none');
             $(".ishiduka-article").css('display', 'none');
-        }else
+        }else{
+            $(".hanashita .triangle").css('display', 'none');
             $(".hanashita-article").css('display', 'none');
+        }
     });
  });
 $(function() {
@@ -42,6 +57,6 @@ $(function() {
 
 $(window).on('click load resize', function(){
     var m_height = $('.ishiduka-article > p:eq(0)', '#voice').height() + $('.ishiduka-article > p:eq(1)', '#voice').height() + $('.ishiduka-article > p:eq(2)', '#voice').height() + $('.ishiduka-article > p:eq(3)', '#voice').height();
-  $('.ishiduka-article', '#voice').css('height', m_height + 90 +'px' );
+  $('.ishiduka-article', '#voice').css('height', m_height + 70 +'px' );
   $('#overview').css('margin-top', m_height + 110 + 'px');
 });
